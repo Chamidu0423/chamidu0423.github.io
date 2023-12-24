f9f78cf8b8e9c0f721cd9b79a2f62d2b77f67840
Chamidu Dilshan <103932928+Chamidu0423@users.noreply.github.com>
2023-12-25 03:49:26 +1000

    added new summery

---
+++ js/auto-type.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var typed = new Typed('.typed-text-output', {
+        strings: ["Web Designer" , "Front End Developer", "Apps Designer", "UI UX Designer"],
+        typeSpeed: 100,
+        backSpeed: 20,
+        smartBackspace: true,
+        loop: true
+    });
+});
+
+    
